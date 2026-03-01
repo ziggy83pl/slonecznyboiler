b776007d53806b3294bf1c5aa10bb16f6fa21a6c
Zbyszek <57629429+ziggy83pl@users.noreply.github.com>
2026-03-01 21:00:46 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sloneczny-bojler-v46';
+const CACHE_NAME = 'sloneczny-bojler-v47';
 const urlsToCache = [
   './',
   './index.html',
@@ -50,6 +50,7 @@ self.addEventListener('fetch', event => {
     );
 
 });
+
 
 
 
